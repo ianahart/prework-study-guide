9973c6cb5f471664f91425fda7245b4f6480a993
Ian Hart <ianhart7790@gmail.com>
2023-10-06 13:12:10 -0400

    call listTopics function after defining selectTopic function

--- assets/script.js
+++ assets/script.js
@@ -7,9 +7,6 @@ function listTopics() {
   }
 }
 
-console.log('Here are the topics we learned through Prework:');
-listTopics();
-
 function selectTopic() {
   if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
@@ -23,5 +20,8 @@ function selectTopic() {
     console.log('Please try again!');
   }
 }
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
